98a456bad7d0b4d952c569bd3b2d0529e97585a5
Anton Sidorov <antosid@gmail.com>
2024-11-21 19:03:15 +0000

    tests: :

--- src/root.zig
+++ src/root.zig
@@ -42,29 +42,30 @@ pub fn updateCommitMessage(allocator: std.mem.Allocator, file_path: []const u8, 
 
 test "updateCommitMessage updates the commit message with the branch name" {
     const allocator = testing.allocator;
+
     const test_dir_rel_path = "test_update_commit_message";
+    const commit_msg_file_name = "COMMIT_MSG";
+    const commit_msg_file_path = test_dir_rel_path ++ "/" ++ commit_msg_file_name;
+
+    const initial_msg = "Initial commit\n";
+    const feature_branch = "feature-branch";
 
     try std.fs.cwd().makeDir(test_dir_rel_path);
     var test_dir = try std.fs.cwd().openDir(
         test_dir_rel_path,
-        .{ .iterate = true },
+        .{},
     );
     defer {
         test_dir.close();
         std.fs.cwd().deleteTree(test_dir_rel_path) catch unreachable;
     }
 
-    const commit_msg_file = try test_dir.createFile("COMMIT_MSG", .{});
+    const commit_msg_file = try test_dir.createFile(commit_msg_file_name, .{});
     defer commit_msg_file.close();
-
-    const commit_msg_file_path = test_dir_rel_path ++ "/COMMIT_MSG";
-
-    const initial_msg = "Initial commit\n";
 
     const len = try commit_msg_file.write(initial_msg);
     try testing.expect(len == initial_msg.len);
 
-    const feature_branch = "feature-branch";
     try updateCommitMessage(allocator, commit_msg_file_path, feature_branch);
 
     const updated_msg = try std.fs.cwd().readFileAlloc(allocator, commit_msg_file_path, 1024 * 1024);
